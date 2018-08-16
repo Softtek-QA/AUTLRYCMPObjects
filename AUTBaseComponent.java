f2e7c66c2f63d2f34ae428a292495146c2e2431b
Administrador <Administrador@AUTVMLRY00010>
2018-08-16 20:03:50 -0300

    Inclusão de componente básico

---
+++ AUTBaseComponent.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package br.lry.components;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.borland.silktest.jtf.BrowserBaseState;
+import com.borland.silktest.jtf.Desktop;
+import com.microfocus.silktest.jtf.*;
+
+/**
+ * 
+ * Componente - Realiza login no sistema:
+ * 
+ * VA : Vendas assistidas
+ * 
+ * @author Softtek-QA
+ *
+ */
+public class AUTBaseComponent {
+	Desktop agent = new Desktop();
+	BrowserBaseState bsConfigBrowser = null;
+	
+	/**
+	 * 
+	 * Inicializa aplicação da aplicaçao VA
+	 * 
+	 */
+	@Before
+	public void autInitWebApplication() {
+		
+		bsConfigBrowser = new BrowserBaseState();
+		agent.executeBaseState(bsConfigBrowser);
+		
+		System.out.println("AUT INFO: INICIALIZANDO APLICAÇÃO WEB");
+		
+	}
+	
+	
+	/**
+	 * 
+	 * Finaliza a aplicação utilizada na automação
+	 * 
+	 */
+	@After
+	public void autCloseApplication() {
+		System.out.println("AUT INFO: FINALIZANDO APLICAÇÃO");			
+	}
+	
+	/**
+	 * 
+	 * Executa procedimentos de login na aplicação - VA
+	 * 
+	 */
+	@Test
+	public void autLoginVA() {
+		
+	}
+	
+	
+	/**
+	 * 
+	 * Construtor padrão da classe
+	 * 
+	 */
+	public AUTBaseComponent() {
+		
+	}
+}
