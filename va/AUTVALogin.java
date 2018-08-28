56add3c47832420ac402549dc33e9c436b495922
fernandaaguiar <fernanda.aguiar@softtek.com>
2018-08-28 10:00:04 -0300

    Update

--- va/AUTVALogin.java
+++ va/AUTVALogin.java
@@ -5,11 +5,9 @@ package br.lry.components.va;
 
 import br.lry.components.AUTVABaseComponent;
 import br.lry.functions.AUTProjectsFunctions.AUTLogMensagem;
-import br.lry.process.AUTSAP01AbastecimentoEstoque;
 import junit.framework.AssertionFailedError;
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestResult;
-import br.lry.components.sap.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,7 +19,7 @@ import org.junit.Test;
  */
 public class AUTVALogin extends AUTVABaseComponent {
 
-	@Test
+	@Before
 	public void autStartLoginDefault() {
 		try {	
 			autGetLogManager().logMensagem("AUT ERROR: LOGIN VA APPLICATION: INIT");
