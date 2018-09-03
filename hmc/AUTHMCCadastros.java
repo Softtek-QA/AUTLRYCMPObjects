e913007a7dcd04498824394ff681eee352cb993f
fernandaaguiar <fernanda.aguiar@softtek.com>
2018-09-03 11:44:15 -0300

    update

--- hmc/AUTHMCCadastros.java
+++ hmc/AUTHMCCadastros.java
@@ -15,47 +15,50 @@ public class AUTHMCCadastros extends AUTHMCLogin {
 	private Desktop AUT_AGENT_SILK4J = new Desktop();
 	
 	
-	public enum autTipeOfCutomer{
-		CUSTUMER,
-		LM_CUSTOMER,
-		lM_JURICAL_PERSON,
-		B2B_CUSTOMER;
-		
-		@Override
-		public String toString() {
-			switch(this) {
-			case CUSTUMER: {
-				return "Customer";
-			}
-			case LM_CUSTOMER: {
-				return "[LMCustomer]";
-			}
-			case lM_JURICAL_PERSON: {
-				return "[LMJuricalPerson]";
-			}
-			case B2B_CUSTOMER: {
-				return "B2B Customer";
-			}
-			}
-			return super.toString();
-		}
-	}
+	String usuario = autGetCurrentParameter(AUT_TABLE_PARAMETERS_NAMES.AUT_HMC_LOGIN, "AUT_USER").toString();
+	String senha = autGetCurrentParameter("AUT_PASSWORD").toString();
+	
+	/*
+	String userID = autGetCurrentParameter("AUT_USER_ID").toString();
+	String userName = autGetCurrentParameter("AUT_USER_NAME").toString();
+	String userEmail = autGetCurrentParameter("AUT_USER_EMAIL").toString();
+	String newPassword = autGetCurrentParameter("AUT_NEW_PASSWORD").toString();
+	*/
 	
 	
-	//String userID = autGetCurrentParameter(AUT_TABLE_PARAMETERS_NAMES.AUT_HMC_LOGIN, "AUT_USER_ID").toString();
-	//String userName = autGetCurrentParameter("AUT_USER_NAME").toString();
-	
-	
+	/*
+	 * CADASTRO DE USUARIO B2B CUSTOMER
+	 * 
+	 */
 	@Test
 	public void autCadastrarUsuarioHMC() {
 		
-		autStartLoginDefault();
+		autStartLoginDefault(usuario, senha);
 		
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.User").click();
 		AUT_AGENT_SILK4J.<DomLink>find("HMC.TelaInicial.Customers").click();
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.New").click();
-				
+		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.B2BCustomer").click();
 		
+		/*
+		//Aba General
+		AUT_AGENT_SILK4J.<DomElement>find("HMC.Menus.General").click();
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.GeneralConfigurations.ID").setText(userID);
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.GeneralConfigurations.Name").setText(userName);
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.GeneralConfigurations.CustomerID").setText(userName);
+		
+		//Aba Addresses
+		AUT_AGENT_SILK4J.<DomElement>find("HMC.Menus.Addresses").click();
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.AddressesConfiguration.EMail").setText(userEmail);
+		
+		//Aba Password
+		AUT_AGENT_SILK4J.<DomElement>find("HMC.Menus.Password").click();
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.PaswordConfiguration.NewPassword").setText(newPassword);
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.PasswordConfiguration.PasswordRepeat").setText(newPassword);
+		
+		//Aba Administration
+		
+		*/
 
 
 	
