eac91e2dc1ffc8db5eb0ecfd9d28cc1fd4a5d732
marcosoliveirastk <marcos.oliveira@softtek.com>
2018-09-06 05:26:44 -0300

    Inclusão de cadastro

--- hmc/AUTHMCCadastros.java
+++ hmc/AUTHMCCadastros.java
@@ -30,7 +30,6 @@ public class AUTHMCCadastros extends AUTHMCLogin {
 	String codigoCategoria = autGetCurrentParameter("AUT_CODIGO_CATEGORIA").toString();
 	String codigoDepartamento = autGetCurrentParameter("AUT_CODIGO_DEPARTAMENTO").toString();
 	String loja = autGetCurrentParameter("AUT_LOJA").toString();
-	String gestor = autGetCurrentParameter("AUT_GESTOR").toString();
 	
 	
 	
@@ -42,72 +41,46 @@ public class AUTHMCCadastros extends AUTHMCLogin {
 	 */
 	@Test
 	public void autCadastrarUsuarioHMC() {
-		
-		autStartLoginDefault(usuario, senha);	
+
+		autStartLoginDefault(usuario, senha);
 		//AUT_AGENT_SILK4J.<DomTextField>find("HMC.Maximizar").click();
 		//AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaLogin.Usuario").click();
 		//AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaLogin.Usuario").setText(usuario);
-		
+
 		//AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaLogin.Senha").click();
 		//AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaLogin.Senha").setText(senha);		
 		//AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaLogin.BotaoLogin").click();
-		
+
 		AUT_AGENT_SILK4J.<DomListBox>find("HMC.TelaInicial.ListaIdiomas").click();
 		AUT_AGENT_SILK4J.<DomListBox>find("HMC.TelaInicial.ListaIdiomas").select("Português do Brasil");
 
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.MenuLateralUsuarios").click();
-				
+
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.Clientes").click();
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.NovoItem").click();
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.SubMenuClienteDropDown").click();
-		
-		
-		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaCadastroClientes.IdUsuario").setText(userID);		
-		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaCadastroClientes.NomeUsuario").setText(userName);		
-		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaCadastroClientes.DescricaoUsuario").setText(userName);		
-		
+
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaCadastroClientes.IdUsuario").setText(userID);
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaCadastroClientes.NomeUsuario").setText(userName);
+		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaCadastroClientes.DescricaoUsuario").setText(userName);
+
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaRemetentes.AbaRemetentes").click();
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaRemetentes.Email").setText(email);
-		
+
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaSenha.AbaSenha").click();
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaSenha.NovaSenha").setText(novaSenha);
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaSenha.Senha").setText(novaSenha);
-		
+
 		AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaAdministracao.AbaAdministracao").click();
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.Canal").setText(tipo);
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.Tipo").setText(canal);
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.UnidadeB2BPadrao").setText(unidadeB2B);
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.Departamento").setText(departamento);
-		
+
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.NumeroDepartamento").setText(departamento);
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.CodigoCategoria").setText(codigoCategoria);
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.CodigoDepartamento").setText(codigoDepartamento);
 		AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.Loja").setText(loja);
-		//AUT_AGENT_SILK4J.<DomTextField>find("HMC.TelaAdministracao.Gestor").setText(gestor);
-		
-		
-		
-		
-		
-		/*
-		//AUT_AGENT_SILK4J.<DomElement>find("HMC.TelaInicial.B2BCustomer").click();
-		
-		//Aba Addresses
-		AUT_AGENT_SILK4J.<DomElement>find("HMC.Menus.Addresses").click();
-		AUT_AGENT_SILK4J.<DomTextField>find("HMC.AddressesConfiguration.EMail").setText(userEmail);
-		
-		
-		//Aba Password
-		AUT_AGENT_SILK4J.<DomElement>find("HMC.Menus.Password").click();
-		AUT_AGENT_SILK4J.<DomTextField>find("HMC.PaswordConfiguration.NewPassword").setText(newPassword);
-		AUT_AGENT_SILK4J.<DomTextField>find("HMC.PasswordConfiguration.PasswordRepeat").setText(newPassword);
-		
-		//Aba Administration
-		AUT_AGENT_SILK4J.<DomElement>find("HMC.Menus.Administrator").click();
-		//AUT_AGENT_SILK4J.<DomElement>find("HMC.AdministratorConfiguration.Type").setText();
-		
-		*/
-	
 	}
 
 	
