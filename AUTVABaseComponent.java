880d6ffa9c1e2887db94e4726a2d92b0f38fbe81
fernandaaguiar <fernanda.aguiar@softtek.com>
2018-10-11 10:24:45 -0300

    Inclusão de método antifraude em novo padrão

--- AUTVABaseComponent.java
+++ AUTVABaseComponent.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import com.borland.silktest.jtf.xbrowser.DomTextField;
 
+import br.lry.components.va.cat018.AUTAntifraude;
 import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
 import br.lry.functions.AUTVAProjectFunctions;
 import br.lry.functions.AUTProjectsFunctions.AUTLogMensagem.AUT_TIPO_MSG_LOG;
@@ -26,6 +27,7 @@ import com.borland.silktest.jtf.xbrowser.DomElement;
 public class AUTVABaseComponent extends AUTBaseComponent {
 	public String AUT_USUARIO_LOGIN_DEFAULT = "";
 	public String AUT_SENHA_LOGIN_DEFAULT = "";
+	AUTAntifraude cat018 = new AUTAntifraude();
 	
 	java.util.HashMap<String,Object> AUT_PARAMETROS_CONFIGURACAO = this.autGetDataFlow().autGetParameter();	
 	
@@ -98,355 +100,23 @@ public class AUTVABaseComponent extends AUTBaseComponent {
 		AUT_AGENT_SILK4J.<AccessibleControl>find("VA02.Fechar").click();
 	}
 	
-	/**
-	 * 
-	 * 
-	 * CMP00001 - CMP00004 - Realizar login com perfil Televenda
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */
-	public boolean CMP00004(java.util.HashMap<String, Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
 	
 	
-	/**
-	 * 
-	 * CMP00003 - Recuperar Carrinho de Compra
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00009(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
 	
 	
-	/**
-	 * 
-	 * CMP00001 - Inserir item no carrinho de compra pelo VA
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00010(java.util.HashMap<String,Object> parametro) {
-		
-		try {
 
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
 
-			return false;
-		}
 
-	}
+
 	
-	
-	/**
-	 * 
-	 * CMP00001 - Criar carrinho
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00013(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
+	public boolean COMPONENTE00001() {
+		return cat018.autVAFlagIgnorarAntifraude();
 	}
 	
 	
 	
-	/**
-	 * 
-	 * CMP00001 - Converter em Pedido
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00014(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
 	
+
 	
-	/**
-	 * 
-	 * CMP00001 - Busca de Cliente Pessoa Física
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00016(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP00005 - Selecionar caixa
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00028(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP00002 - Cartão de Credito
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00039(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-		
-	
-	
-	/**
-	 * 
-	 * CMP000012 - Cartão de Credito e Voucher
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00049(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP000013 - Cartão de Credito e Vale Troca
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00050(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP00001 - Pedido em edição
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00051(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP00001 - Realizar logOff
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00052(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP00003 - Busca de pedido para Aprovação Antifraude ou Desconto
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00065(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	/**
-	 * 
-	 * CMP00001 - Seleção de loja Televendas
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00072(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP00002 - Confirmação de Login
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00073(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
-	
-	
-	/**
-	 * 
-	 * CMP00003 - Ignorar Antifraude
-	 * 
-	 * @param parametro - Parametros de entrada do sistema
-	 * @return
-	 */	
-	public boolean CMP00074(java.util.HashMap<String,Object> parametro) {
-		
-		try {
-
-			return true;
-		}
-		
-		catch(java.lang.Exception e) {
-
-			return false;
-		}
-
-	}
 
 }
