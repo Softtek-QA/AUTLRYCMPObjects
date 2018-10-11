be6bd3c78277ebf40e30585e20356b7a96820d86
marcosoliveirastk <marcos.oliveira@softtek.com>
2018-10-11 18:19:59 -0300

    Alteração para correção de merge

--- va/cat018/AUTTrocarLoja.java
+++ va/cat018/AUTTrocarLoja.java
@@ -15,21 +15,4 @@ public class AUTTrocarLoja extends AUTVABaseComponent {
 	 * @param lojaSelecionada - Loja selecionada
 	 */
 	
-	public boolean autTrocaDeLoja(String usuario, String senha, AUT_VA_SELECAO_LOJA lojaSelecionada) {
-		try {
-			autLoginVATelevendas(usuario, senha);
-			AUT_AGENT_SILK4J.<DomElement>find("VA.TelaInicialLoja.BotaoLoja").click();
-			AUT_AGENT_SILK4J.<DomElement>find("VA.Televendas.SelecionarLoja").click();
-			AUT_AGENT_SILK4J.<DomElement>find("VA.Televendas.CampoSelecaoLoja").click();
-			AUT_AGENT_SILK4J.<DomTextField>find("VA.Televendas.CampoSelecaoLoja").typeKeys(lojaSelecionada.toString());
-			AUT_AGENT_SILK4J.<DomTextField>find("VA.Televendas.CampoSelecaoLoja").typeKeys("\n");
-			AUT_AGENT_SILK4J.<DomButton>find("VA.Televendas.BotaoEscolher").click();
-			return true;
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-			return false;
-		}
-	}
 }
