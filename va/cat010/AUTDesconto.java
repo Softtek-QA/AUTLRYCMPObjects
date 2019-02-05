b91234c95ae1d3a672f0a4ef538ed978a1e7f1bb
Administrador <Administrador@AUTVMLRY00009>
2019-02-04 22:01:17 -0200

    ENTREGAS 6-10 - SINCRONIZAÇÃO DE CORREÇÃO FRAMEWORK

--- va/cat010/AUTDesconto.java
+++ va/cat010/AUTDesconto.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import com.borland.silktest.jtf.xbrowser.BrowserApplication;
+
 import com.borland.silktest.jtf.xbrowser.DomElement;
 import com.borland.silktest.jtf.xbrowser.DomListBox;
 import com.borland.silktest.jtf.xbrowser.DomRadioButton;
@@ -12,23 +12,19 @@ import com.borland.silktest.jtf.xbrowser.DomTextField;
 
 import br.lry.components.AUTBaseComponent;
 import br.lry.components.va.cat003.Item;
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
-import com.borland.silktest.jtf.common.types.ItemIdentifier;
-import com.borland.silktest.jtf.win32.AccessibleControl;
-import org.junit.Test;
-import com.borland.silktest.jtf.xbrowser.DomButton;
+
 
 public class AUTDesconto extends AUTBaseComponent {
 
-
+	
 	/**
 	 * Desconto no pedido
 	 * @author Softtek - QA
 	 *
 	 */
 
-
+	
 	/**
 	 * Listagem dos Tipos de Desconto
 	 * @author Softtek - QA
@@ -37,7 +33,7 @@ public class AUTDesconto extends AUTBaseComponent {
 	public static enum AUT_TIPO_DESCONTO{
 		REAIS,
 		PORCENTAGEM;
-
+		
 		@Override
 		public String toString() {
 			// TODO Auto-generated method stub
@@ -52,8 +48,8 @@ public class AUTDesconto extends AUTBaseComponent {
 			return super.toString();
 		}
 	}
-
-
+	
+	
 	/**
 	 * Listagem dos Tipos de Motivo para desconto de frete
 	 * @author Softtek - QA
@@ -66,9 +62,9 @@ public class AUTDesconto extends AUTBaseComponent {
 		AVARIA_NO_PRODUTO,
 		PRODUTO_COM_ALTO_VALOR_AGREGADO,
 		RUPTURA_DE_PRODUTO;
-
-
-
+		
+		
+		
 		@Override
 		public String toString() {
 			// TODO Auto-generated method stub
@@ -95,13 +91,13 @@ public class AUTDesconto extends AUTBaseComponent {
 			return super.toString();
 		}
 	}
-
+	
 	public enum AUT_VA_MOTIVO{
 		ESPECIAL,
 		SALDO,
 		ERRO_NO_PRECO,
 		SITE;
-
+		
 		@Override
 		public String toString() {
 			// TODO Auto-generated method stub
@@ -122,8 +118,8 @@ public class AUTDesconto extends AUTBaseComponent {
 			return super.toString();
 		}
 	}
-
-
+	
+	
 	/**
 	 * Desconto sobre o valor da seção 
 	 * @param parametros - Desconto 
@@ -131,10 +127,10 @@ public class AUTDesconto extends AUTBaseComponent {
 	 */
 	public boolean autDescontoSecao(java.util.HashMap parametros) {			
 		try {
-
+			
 			AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.ValorSecao").setFocus();
 			AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.ValorSecao").click();
-
+			
 			if ( parametros.get("AUT_TIPO_DESCONTO").toString() == AUT_TIPO_DESCONTO.REAIS.toString()) {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
@@ -145,25 +141,25 @@ public class AUTDesconto extends AUTBaseComponent {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 			}
-
+			
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_SECAO").setFocus();
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_SECAO").click();
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_SECAO").select(parametros.get("AUT_MOTIVO").toString());
 			AUT_AGENT_SILK4J.<DomTextField>find("VA.Desconto.DescontoPorcentagem_SECAO").click();
 			AUT_AGENT_SILK4J.<DomTextField>find("VA.Desconto.DescontoPorcentagem_SECAO").setText(parametros.get("AUT_DESCONTO").toString());
 
-
+			
 			return true;
-		}
+	}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 			return false;
 		}
-
+	
 	}
-
-
+	
+	
 	/**
 	 * Desconto sobre o valor do Item
 	 * @param parametros - Desconto 
@@ -174,7 +170,7 @@ public class AUTDesconto extends AUTBaseComponent {
 
 			AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.ValorItem").setFocus();
 			AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.ValorItem").click();
-
+			
 			if ( parametros.get("AUT_TIPO_DESCONTO").toString() == AUT_TIPO_DESCONTO.REAIS.toString()) {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
@@ -185,7 +181,7 @@ public class AUTDesconto extends AUTBaseComponent {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 			}
-
+						
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_ITEM").setFocus();
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_ITEM").click();
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_ITEM").select(parametros.get("AUT_MOTIVO").toString());
@@ -193,16 +189,16 @@ public class AUTDesconto extends AUTBaseComponent {
 			AUT_AGENT_SILK4J.<DomTextField>find("VA.Desconto.DescontoPorcentagem_ITEM").typeKeys(parametros.get("AUT_DESCONTO").toString());
 
 			return true;
-		}
+	}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 			return false;
 		}
-
+	
 	}
-
-
+	
+	
 	/**
 	 * Desconto sobre o valor do Total
 	 * @param parametros - Desconto 
@@ -212,7 +208,7 @@ public class AUTDesconto extends AUTBaseComponent {
 		try {
 			AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.ValorTotal").setFocus();
 			AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.ValorTotal").click();
-
+			
 			if ( parametros.get("AUT_TIPO_DESCONTO").toString() == AUT_TIPO_DESCONTO.REAIS.toString()) {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
@@ -223,14 +219,14 @@ public class AUTDesconto extends AUTBaseComponent {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 			}
-
+			
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_TOTAL").setFocus();
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_TOTAL").click();
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_TOTAL").select(parametros.get("AUT_MOTIVO").toString());
 			AUT_AGENT_SILK4J.<DomTextField>find("VA.Desconto.DescontoPorcentagem_TOTAL").click();
 			AUT_AGENT_SILK4J.<DomTextField>find("VA.Desconto.DescontoPorcentagem_TOTAL").typeKeys(parametros.get("AUT_DESCONTO").toString());
 
-
+			
 			return true;
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -240,9 +236,9 @@ public class AUTDesconto extends AUTBaseComponent {
 
 
 	}
+	
 
-
-
+	
 	/**
 	 * Desconto sobre o valor do Frete
 	 * @param parametros - Desconto 
@@ -250,14 +246,14 @@ public class AUTDesconto extends AUTBaseComponent {
 	 */
 	public boolean autDescontoFrete(java.util.HashMap parametros) {
 		try {
-
+			
 			AUT_AGENT_SILK4J.<DomRadioButton>find("VA.FluxoSaida.Entrega").select();
 			AUT_AGENT_SILK4J.<DomElement>find("VA.FluxoSaida.ListaEndereco").click();
 			AUT_AGENT_SILK4J.<DomElement>find("VA.FluxoSaida.PopUpFreteAdicional").click();
 			DomListBox listComboEntrega= AUT_AGENT_SILK4J.<DomListBox>find("VA.ConfirmacaoLogin.Turno");
 			selectValor(listComboEntrega);
 			AUT_AGENT_SILK4J.<DomElement>find("VA.FluxoSaida.Frete").click();
-
+			
 			if ( parametros.get("AUT_TIPO_DESCONTO").toString() == AUT_TIPO_DESCONTO.REAIS.toString()) {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
@@ -268,16 +264,16 @@ public class AUTDesconto extends AUTBaseComponent {
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 			}
-
-			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_FRETE").select(parametros.get("AUT_MOTIVO").toString());
+			
+	        AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.Motivo_FRETE").select(parametros.get("AUT_MOTIVO").toString());
 			AUT_AGENT_SILK4J.<DomTextField>find("VA.Desconto.Desconto_FRETE").click();
 			AUT_AGENT_SILK4J.<DomTextField>find("VA.Desconto.Desconto_FRETE").typeKeys(parametros.get("AUT_DESCONTO").toString());
 			AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.Confirmar").click();
 
 
+			
 
-
-
+			
 			return true;
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -287,25 +283,25 @@ public class AUTDesconto extends AUTBaseComponent {
 
 
 	}
-
-
-
+	
+	
+	
 	public boolean autPopUpConfirmacao() {			
 		try {
 			AUT_AGENT_SILK4J.<DomListBox>find("VA.Desconto.PopUpSim").click();
-
+		
 			return true;
-		}
+	}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 			return false;
 		}
-
+	
 	}
-
-
-
+	
+	
+	
 	/**
 	 * Desconto sobre o valor dos Itens
 	 * @param parametros - Desconto 
@@ -313,7 +309,7 @@ public class AUTDesconto extends AUTBaseComponent {
 	 */
 	public boolean autDescontoItens(java.util.HashMap parametros) {			
 		try {
-
+			
 			HashMap<Integer, ArrayList<Item>> item = Item.carregaItens();
 
 			int quantidadeItens = (int) parametros.get("AUT_QUANTIDADE_ITENS");
@@ -330,58 +326,144 @@ public class AUTDesconto extends AUTBaseComponent {
 					}
 
 
-					AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@data-entry='"+contador+"']").click();;
+			AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@data-entry='"+contador+"']").click();;
 
-
-					if ( material.getTipoDesconto().toString() == AUT_TIPO_DESCONTO.REAIS.toString()) {
-						AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
-						AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
-					}
-
-					else if (material.getTipoDesconto().toString() == AUT_TIPO_DESCONTO.PORCENTAGEM.toString()) {
-						AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
-						AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
-						AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
-					}
-
-
-					AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@id='item-"+contador+"']//DIV[@id='item-discount']//SELECT[@class='select']").setFocus();
-					AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@id='item-"+contador+"']//DIV[@id='item-discount']//SELECT[@class='select']").click();
-					AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//DIV[@id='item-"+contador+"']//DIV[@id='item-discount']//SELECT[@class='select']").select(material.getMotivoDesconto().toString());
-					AUT_AGENT_SILK4J.<DomTextField>find("VA.AtualizacaoDados.//INPUT[@id='value-discount-"+contador+"']").click();
-					AUT_AGENT_SILK4J.<DomTextField>find("VA.AtualizacaoDados.//INPUT[@id='value-discount-"+contador+"']").typeKeys(material.getValorDesconto().toString());
-
-					contador++;
-
-				}
+			
+			if ( material.getTipoDesconto().toString() == AUT_TIPO_DESCONTO.REAIS.toString()) {
+				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
+				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
 			}
-			return true;
+
+			else if (material.getTipoDesconto().toString() == AUT_TIPO_DESCONTO.PORCENTAGEM.toString()) {
+				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").setFocus();
+				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
+				AUT_AGENT_SILK4J.<DomElement>find("VA.Desconto.EscolherTipoDesconto_ITEM").click();
+			}
+					
+			
+			AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@id='item-"+contador+"']//DIV[@id='item-discount']//SELECT[@class='select']").setFocus();
+			AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@id='item-"+contador+"']//DIV[@id='item-discount']//SELECT[@class='select']").click();
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//DIV[@id='item-"+contador+"']//DIV[@id='item-discount']//SELECT[@class='select']").select(material.getMotivoDesconto().toString());
+			AUT_AGENT_SILK4J.<DomTextField>find("VA.AtualizacaoDados.//INPUT[@id='value-discount-"+contador+"']").click();
+			AUT_AGENT_SILK4J.<DomTextField>find("VA.AtualizacaoDados.//INPUT[@id='value-discount-"+contador+"']").typeKeys(material.getValorDesconto().toString());
+
+			contador++;
+			
+			}
 		}
+			return true;
+	}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 			return false;
 		}
-
+	
 	}
-
-
+	
+	
 	/**
 	 * Desconto sobre o valor dos Itens
 	 * @param parametros - Desconto 
 	 * @return - Verdadeiro para desconto devidamente aplicado 
 	 */
-	@SuppressWarnings("null")
 	public boolean autDescontoSecoes(java.util.HashMap parametros) {			
 		try {
+					
 
+    		ArrayList<String> teste1 = new ArrayList<String>();
+    		int contador = 0;
+    		int cont = 0;
+			int contadorExpressao = 1;
+			
+			List<DomElement> txtArticleNovos =  AUT_AGENT_SILK4J.<DomElement>findAll("//BrowserApplication//STRONG[@class='*price-tag-detail']");
+			List<DomElement> txtArticle =  AUT_AGENT_SILK4J.<DomElement>findAll("//BrowserApplication//DomElement[@class='price-tag']");
+            
+
+            for(DomElement teste: txtArticleNovos) {
+            	System.out.print("Descobrindo as listas"+teste.getText());
+            	String a = teste.getText().substring(0, 2);
+            	System.out.println("Saida da secao"+a);
+            	teste1.add(a);
+            }
+            
+
+            
+            for (DomElement title : txtArticle) {
+                System.out.print("Lista de rows"+title.getText());
+                System.out.println("Tags"+title.getText());
+				java.util.regex.Pattern padrao = java.util.regex.Pattern.compile(".*Seção.*");
+				java.util.regex.Matcher analise = padrao.matcher(title.getText());
+				contador ++;
+				
+				if(analise.find()) {			
+					
+		            for (int i=0; i < teste1.size(); i++) {
+		            	System.out.println("Saida do numero da seção"+teste1.get(i));
+		         
+		            
+		            
+
+					if (contador ==4) {
+						
+						cont = 3;
+					}
+					
+					else {
+						cont =2;
+					}
+					
+	        		
+	        		if ((int) parametros.get("AUT_QUANTIDADE_SECAO") >= contadorExpressao)  {
+	    			
+	        		title.getText().length();
+	        		
+	        		if(title.getText().length() ==26) {
+			        	String res = title.getText().substring(16, 21);
+		        		AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@id='section-"+teste1.get(i)+"']["+cont+"]//DIV[@textContents='Total Seção:']//SPAN[@class='price-currency']").click(); 		    			
+		        		AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//DIV[@id='section-"+teste1.get(i)+"']["+cont+"]//SELECT[@class='select']").select(parametros.get("AUT_MOTIVO").toString()); 
+		        		AUT_AGENT_SILK4J.<DomTextField>find("VA.AtualizacaoDados.//INPUT[@id='value-discount-"+teste1.get(i)+"']").typeKeys(parametros.get("AUT_DESCONTO").toString()); 		        		
+		        		contador =0;
+		    			System.out.println("Valor do substr"+res);
+	        		}
+	        		
+	        		if(title.getText().length() == 24) {
+			        	String res = title.getText().substring(16, 19);
+			        	
+			        	//BrowserApplication//BrowserWindow//DIV[@id='section-13'][2]//DIV[@textContents='Total Seção:']//SPAN[@class='price-currency']
+		        		AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@id='section-"+teste1.get(i)+"']["+cont+"]//DIV[@textContents='Total Seção:']//SPAN[@class='price-currency']").click(); 		    			
+		        		AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//DIV[@id='section-"+teste1.get(i)+"']["+cont+"]//SELECT[@class='select']").select(parametros.get("AUT_MOTIVO").toString()); 
+		        		AUT_AGENT_SILK4J.<DomTextField>find("VA.AtualizacaoDados.//INPUT[@id='value-discount-"+teste1.get(i)+"']").typeKeys(parametros.get("AUT_DESCONTO").toString()); 		        		
+		        		contador =0;
+		    			System.out.println("Valor do substr"+res);
+	        		}
+	        		
+	        		if(title.getText().length() == 23) {
+			        	String res = title.getText().substring(16, 18);
+		        		AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.//DIV[@id='section-"+teste1.get(i)+"']["+cont+"]//DIV[@textContents='Total Seção:']//SPAN[@class='price-currency']").click(); 		    			
+		        		AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//DIV[@id='section-"+teste1.get(i)+"']["+cont+"]//SELECT[@class='select']").select(parametros.get("AUT_MOTIVO").toString()); 
+		        		AUT_AGENT_SILK4J.<DomTextField>find("VA.AtualizacaoDados.//INPUT[@id='value-discount-"+teste1.get(i)+"']").typeKeys(parametros.get("AUT_DESCONTO").toString()); 		        		
+		        		contador =0;
+		    			System.out.println("Valor do substr"+res);
+	        		}
+	        			
+
+	    			contadorExpressao ++;
+					
+				}	
+		      }
+	
+			}
+            
+        }
+               		
 			return true;
-		}
+	}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 			return false;
 		}
-
+	
 	}	
 }
